4435f3a15f20483eff2089024c13cffecf5ec604
basantuk2024 <basantuk2024@gmail.com>
2025-09-05 10:44:32 +0100

    test2

--- modules/irsaapps3landing-roles/dev.tfvars
+++
@@ -1,4 +0,0 @@
-role_name   = "svc-edp-astro-irsa-dev"
-environment = "dev"
-aws_region  = "us-east-1"
-product_id  = "124930443440-apps3landing-roles"
--- modules/irsaapps3landing-roles/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "role_name" {
-  value = aws_servicecatalog_provisioned_product.irsa_role.name
-}
-
-output "provisioned_product_id" {
-  value = aws_servicecatalog_provisioned_product.irsa_role.id
-}
