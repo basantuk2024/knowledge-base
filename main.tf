3ba62eebbc7704ae960880e994d1e07c49ce40b6
basantuk2024 <basantuk2024@gmail.com>
2025-09-05 10:45:40 +0100

    test2

--- main.tf
+++
@@ -1,7 +0,0 @@
-module "irsaapps3landing_roles" {
-  source      = "./modules/irsaapps3landing-roles"
-  role_name   = "svc-edp-astro-irsa-${var.environment}"
-  environment = var.environment
-  product_id  = var.product_id
-  aws_region  = var.aws_region
-}
